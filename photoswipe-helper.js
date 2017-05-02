ebda1e9f35ea54a8d2fae768fbbd20911e0a04e5
Houke <houke@users.noreply.github.com>
2017-05-02 13:31:26 +0200

    Update photoswipe-helper.js
    Allow video iframes in PhotoSwipe slider

--- photoswipe-helper.js
+++ photoswipe-helper.js
@@ -11,25 +11,32 @@ var ps = (function($){
       var $el = $(this),
         size = $el.data('size').split('x'),
         caption;
+      
+      if($el.attr('data-type') == 'video'){
+				galleryItems.push({
+					type: 'video',
+					html: '<div class="pswp__video-holder"><div class="pswp__video"><iframe width="420" height="315" data-src="https://www.youtube.com/embed/'+$el.attr('data-id')+'?autoplay=1"></iframe></div></div>'
+				});
+			}else{
+        if( $el.next().is('.wp-caption-text') ) {
+          // image with caption
+          caption = $el.next().text();
+        } else if( $el.parent().next().is('.wp-caption-text') ) {
+          // gallery icon with caption
+          caption = $el.parent().next().text();
+        } else {
+          caption = $el.attr('title');
+        }
 
-      if( $el.next().is('.wp-caption-text') ) {
-        // image with caption
-        caption = $el.next().text();
-      } else if( $el.parent().next().is('.wp-caption-text') ) {
-        // gallery icon with caption
-        caption = $el.parent().next().text();
-      } else {
-        caption = $el.attr('title');
+        galleryItems.push({
+          src: $el.attr('data-src'),
+          w: parseInt(size[0], 10),
+          h: parseInt(size[1], 10),
+          title: caption,
+          msrc: $el.find('img').attr('src'),
+          el: $el
+        });
       }
-
-      galleryItems.push({
-        src: $el.attr('data-src'),
-        w: parseInt(size[0], 10),
-        h: parseInt(size[1], 10),
-        title: caption,
-        msrc: $el.find('img').attr('src'),
-        el: $el
-      });
       if( el === $el.get(0) ) {
         index = i;
       }
@@ -67,6 +74,15 @@ var ps = (function($){
 
     // Pass data to PhotoSwipe and initialize it
     gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.listen('beforeChange', function(e) {
+			if(gallery.currItem.type == 'video'){
+				var iframe = $('.pswp__video-holder').find('iframe');
+				iframe.attr('src', iframe.data('src'));
+			}else{
+				var iframe = $('.pswp__video-holder').find('iframe').attr('src', '');
+			}
+		});
+		gallery.listen('destroy', function() { $('.pswp__video-holder').remove(); });
     gallery.init();
   };
 
